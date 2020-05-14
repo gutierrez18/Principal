1fe74e2a6d944e1557cbb5e1165f8356a5173395
asus <asus@DESKTOP-SGU2795>
2020-05-14 10:25:28 -0500

    Agregar archivos de proyecto.

---
+++ Principal/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Principal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btnsalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btnsalir
+            // 
+            this.Btnsalir.BackColor = System.Drawing.Color.Red;
+            this.Btnsalir.ForeColor = System.Drawing.Color.White;
+            this.Btnsalir.Location = new System.Drawing.Point(674, 270);
+            this.Btnsalir.Name = "Btnsalir";
+            this.Btnsalir.Size = new System.Drawing.Size(71, 28);
+            this.Btnsalir.TabIndex = 0;
+            this.Btnsalir.Text = "Salir";
+            this.Btnsalir.UseVisualStyleBackColor = false;
+            this.Btnsalir.Click += new System.EventHandler(this.Btnsalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 303);
+            this.Controls.Add(this.Btnsalir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btnsalir;
+    }
+}
+
